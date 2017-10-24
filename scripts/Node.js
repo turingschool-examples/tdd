f33de56949f0bfd6d71e3d0afe4f224d2dfd0f07
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-10-24 08:38:45 -0600

    updated node class

--- scripts/Node.js
+++ scripts/Node.js
@@ -1,5 +1,7 @@
 export default class Node {
-  constructor () {
-    
+  constructor (data = null, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
   }
 }
